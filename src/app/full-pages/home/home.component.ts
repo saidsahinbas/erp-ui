42f25689c48cee19d0796d70fc6f56c18bc1a31c
Said Sahinbas <said.sahinbas@profeed.app>
2024-12-22 04:40:12 +0300

    yetkiler ile ilgili sayfalara erişme tamamlandı.

--- src/app/full-pages/home/home.component.ts
+++ src/app/full-pages/home/home.component.ts
@@ -17,15 +17,37 @@ export class HomeComponent implements OnInit {
     quality: false
   };
 
-  constructor(private router: Router,
-              private authService: AuthenticationService) {
+  permissions: any = {}; // Object to store screen permissions
 
-  }
+  constructor(
+    private router: Router,
+    private authService: AuthenticationService
+  ) {}
 
   ngOnInit() {
     if (!this.authService.isAuthenticated()) {
       this.router.navigate(['/login']);
     }
+
+    const userSession = sessionStorage.getItem('userSession');
+    if (userSession) {
+      const parsedSession = JSON.parse(userSession);
+      this.permissions = this.mapPermissions(parsedSession.screens);
+      console.log(parsedSession)
+    }
+  }
+
+  /**
+   * Converts the screens array into a key-value map for easier access.
+   * @param screens Array of permissions from userSession.
+   * @returns An object where the keys are screen names and values are permission objects.
+   */
+  mapPermissions(screens: any[]): any {
+    const permissionsMap: any = {};
+    screens.forEach(screen => {
+      permissionsMap[screen.screenName] = screen;
+    });
+    return permissionsMap;
   }
 
   toggleDropdown(dropdownName: string): void {
@@ -38,4 +60,12 @@ export class HomeComponent implements OnInit {
     return this.dropdownStates[dropdownName];
   }
 
+  /**
+   * Checks if a user has the `read` permission for a specific screen.
+   * @param screenName The name of the screen.
+   * @returns True if the user has the `read` permission, otherwise false.
+   */
+  hasReadPermission(screenName: string): boolean {
+    return this.permissions[screenName]?.read || false;
+  }
 }
